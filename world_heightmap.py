76507d3811cc2b354b033783c14d0aecd039d6e4
Adian Kozlica <kozlicaadian@gmail.com>
2025-05-26 17:08:31 +0200

    feat: add support for rivers, and create seperate upscale func

--- world_heightmap.py
+++ world_heightmap.py
@@ -16,6 +16,7 @@ from PyQt6.QtCore import QLocale
 from pathlib import Path
 from pyproj import CRS
 from PIL import Image
+from upscale import upscale_func
 
 import os
 import subprocess
@@ -26,13 +27,17 @@ import numpy as np
 ROOT = Path(__file__).parent
 ETOPO1_PATH = str(ROOT / "data/etopo1.tif")
 WATER_MASK_PATH = str(ROOT / "data/gshhs_land_water_mask_3km_i.tif")
-
+RIVERS_PATH = str(ROOT / "data/rivers.tif")
 
 def clip(west: str, south: str, east: str, north: str):
     src_tmp_file = tempfile.NamedTemporaryFile(
         suffix=".tif", delete=False
     )
     water_mask_tmp_file = tempfile.NamedTemporaryFile(
+        suffix=".tif", delete=False
+    )
+
+    rivers_tmp_file = tempfile.NamedTemporaryFile(
         suffix=".tif", delete=False
     )
 
@@ -61,7 +66,20 @@ def clip(west: str, south: str, east: str, north: str):
         ]
     )
 
-    return src_tmp_file.name, water_mask_tmp_file.name
+    subprocess.call(
+        [
+            "rio",
+            "clip",
+            RIVERS_PATH,
+            rivers_tmp_file.name,
+            "--overwrite",
+            "--geographic",
+            "--bounds",
+            f"{west} {south} {east} {north}",
+        ]
+    )
+
+    return src_tmp_file.name, water_mask_tmp_file.name, rivers_tmp_file.name
 
 
 def xy_to_lat_lon(x: int, y: int, src):
@@ -84,13 +102,15 @@ def xy_to_lat_lon(x: int, y: int, src):
 def transform(
     src_path: str,
     water_path: str,
+    river_path: str,
     out: str,
     make_water_elevation_always_zero=False,
     min_elevation=float("-inf"),
 ):
-    with rasterio.open(src_path) as src, rasterio.open(water_path) as water_mask:
+    with rasterio.open(src_path) as src, rasterio.open(water_path) as water_mask, rasterio.open(river_path) as river_src:
         data = src.read(1)
         water_data = water_mask.read(1)
+        river_data = river_src.read(1)
         max_elevation = np.nanmax(data)
 
         with Image.new("RGB", (water_mask.width, water_mask.height)) as img:
@@ -109,10 +129,21 @@ def transform(
                     elev = int(255 * (elev / max_elevation))
                     pixels[x, y] = (elev, elev, elev)
 
-            img.save(out)
+            upscaled = upscale_func(img, 2).resize((river_src.width, river_src.height))
+
+            with Image.new('RGB', (river_src.width, river_src.height)) as river_img:
+                upscaled_pixels = upscaled.load()
+                
+                for y in range(river_src.height):
+                    for x in range(river_src.width):
+                        if river_data[y, x] != 0:
+                            upscaled_pixels[x, y] = (0, 0, 0)
+            
+            upscaled.save(out)
 
     os.remove(src_path)
     os.remove(water_path)
+    os.remove(river_path)
 
 
 class MainWidget(QWidget):
@@ -152,10 +183,11 @@ class MainWidget(QWidget):
         if not file_path.endswith('.bmp'):
             file_path += '.bmp'
 
-        src, water = clip(west, south, east, north)
+        src, water, river = clip(west, south, east, north)
         transform(
             src,
             water,
+            river,
             file_path,
             self.__make_water_elevation_always_zero.isChecked(),
             min_elevation,
